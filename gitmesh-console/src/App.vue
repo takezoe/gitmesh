c184e0293075dd1582cb29eb2ab3b20671225006
Naoki Takezoe <takezoe@gmail.com>
2018-02-19 00:51:35 +0900

    Fix bootstrap navbar

--- gitmesh-console/src/App.vue
+++ gitmesh-console/src/App.vue
@@ -2,11 +2,9 @@
   <div id="app">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <a class="navbar-brand" href="#"><img src="./assets/git.png" width="32" height="32"/></a>
-      <!--
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      -->
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item" :class="{active: $route.path == '/nodes'}">
--- gitmesh-console/src/main.js
+++ gitmesh-console/src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import 'bootstrap'
 require('bootstrap/dist/css/bootstrap.css')
 
 Vue.config.productionTip = false
