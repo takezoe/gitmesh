745b25d46bf2550fd431564aabbc6707da1763a8
Naoki Takezoe <takezoe@gmail.com>
2018-05-07 18:02:19 +0900

    Fix compilation error

--- gitmesh-repository-server/src/main/scala/com/github/takezoe/gitmesh/repository/util/Config.scala
+++ gitmesh-repository-server/src/main/scala/com/github/takezoe/gitmesh/repository/util/Config.scala
@@ -1,6 +1,5 @@
 package com.github.takezoe.gitmesh.repository.util
 
-import com.github.takezoe.resty.HttpClientConfig
 import com.typesafe.config.ConfigFactory
 
 import scala.collection.JavaConverters._
@@ -8,8 +7,7 @@ import scala.collection.JavaConverters._
 case class Config(
   url: String,
   directory: String,
-  controllerUrl: Seq[String]//,
-//  httpClient: HttpClientConfig
+  controllerUrl: Seq[String]
 )
 
 object Config {
@@ -21,13 +19,7 @@ object Config {
     Config(
       url           = c.getString("gitmesh.url"),
       directory     = c.getString("gitmesh.directory"),
-      controllerUrl = c.getStringList("gitmesh.controllerUrl").asScala//,
-//      httpClient    = HttpClientConfig(
-//        maxRetry      = c.getInt("resty.httpClient.maxRetry"),
-//        retryInterval = c.getInt("resty.httpClient.retryInterval"),
-//        maxFailure    = c.getInt("resty.httpClient.maxFailure"),
-//        resetInterval = c.getInt("resty.httpClient.resetInterval")
-//      )
+      controllerUrl = c.getStringList("gitmesh.controllerUrl").asScala
     )
   }
 }
