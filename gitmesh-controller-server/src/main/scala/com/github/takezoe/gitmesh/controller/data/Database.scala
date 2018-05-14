a08bbf91689f41c3b4c2581ed865d746ce26d3d0
Naoki Takezoe <takezoe@gmail.com>
2018-05-14 18:08:21 +0900

    Remove commented out code

--- gitmesh-controller-server/src/main/scala/com/github/takezoe/gitmesh/controller/data/Database.scala
+++ gitmesh-controller-server/src/main/scala/com/github/takezoe/gitmesh/controller/data/Database.scala
@@ -6,30 +6,7 @@ import doobie.hikari._, doobie.hikari.implicits._
 
 object Database {
 
-//  private var dataSource: HikariDataSource = null
   var xa: HikariTransactor[IO] = null
-
-//  private def createDataSource(config: Config.DatabaseConfig): HikariTransactor[IO] = {
-//    HikariTransactor.newHikariTransactor[IO](
-//      config.driver,
-//      config.url,
-//      config.user,
-//      config.password
-//    ).unsafeRunSync()
-//
-////    new HikariDataSource(new HikariConfig().unsafeTap { hikariConfig =>
-////      hikariConfig.setDriverClassName(config.driver)
-////      hikariConfig.setJdbcUrl(config.url)
-////      hikariConfig.setUsername(config.user)
-////      hikariConfig.setPassword(config.password)
-////      hikariConfig.setAutoCommit(false)
-////      config.connectionTimeout.foreach(hikariConfig.setConnectionTimeout)
-////      config.idleTimeout.foreach(hikariConfig.setIdleTimeout)
-////      config.maxLifetime.foreach(hikariConfig.setMaxLifetime)
-////      config.minimumIdle.foreach(hikariConfig.setMinimumIdle)
-////      config.maximumPoolSize.foreach(hikariConfig.setMaximumPoolSize)
-////    })
-//  }
 
   def initializeDataSource(config: Config.DatabaseConfig): Unit = {
     xa = (for {
@@ -53,28 +30,5 @@ object Database {
   def closeDataSource(): Unit = {
     xa.shutdown.unsafeRunSync()
   }
-
-//  def withTransaction[T](conn: Connection)(f: => T): T = {
-//    conn.setAutoCommit(false)
-//    try {
-//      val result = f
-//      conn.commit()
-//      result
-//    } catch {
-//      case e: Exception =>
-//        conn.rollback()
-//        throw e
-//    }
-//  }
-//
-//  def withConnection[T](f: (Connection) => T): T = {
-//    using(xa.connect((): Unit).unsafeRunSync()){ conn =>
-//      try {
-//        f(conn)
-//      } finally ignoreException {
-//        conn.close()
-//      }
-//    }
-//  }
 
 }
