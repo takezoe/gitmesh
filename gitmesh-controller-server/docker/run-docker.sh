19306432c9f8b398d88e372d8daff7685fdf9c00
Naoki Takezoe <takezoe@gmail.com>
2018-09-14 00:27:17 +0900

    Remove run-docker.sh

--- gitmesh-controller-server/docker/run-docker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export VERSION=`cat ../build.sbt | grep version | sed -e 's/[^"]*"\([^"]*\)".*/\1/'`
-docker run -it -p 8081:8080 --rm gitmesh-controller-server:$VERSION
--- gitmesh-repository-server/docker/run-docker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export VERSION=`cat ../build.sbt | grep version | sed -e 's/[^"]*"\([^"]*\)".*/\1/'`
-docker run -it -p 8082:8080 -v repos:/repos --rm gitmesh-repository-server:$VERSION
